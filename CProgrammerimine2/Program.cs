c1f75e28f04707cfc2c5708cbe5dd7a6959cc3c8
Admin <Admin@DESKTOP-HSVVIDV>
2022-02-08 20:06:01 +0200

    Add project files.

---
+++ CProgrammerimine2/Program.cs
@@ -0,0 +1,28 @@
+﻿                // Console.WriteLine("Neighbors is out");
+                //}
+                //int plosad = 0;
+                //Console.WriteLine("Какая длина стены a?");
+                //int dlina1 = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Какая длина стены b?");
+                //int dlina2 = int.Parse(Console.ReadLine());
+                //plosad = dlina1 * dlina2;
+                //Console.WriteLine($"Площадь пола = {plosad}");
+                //Console.WriteLine("Хотели бы вы сделать ремонт?");
+                //string remont = Console.ReadLine();
+                //if (remont.ToUpper() == "YES" || remont.ToUpper() == "ДА")
+                //{
+                //    Console.WriteLine("Сколько стоит квадратный метр?");
+                //    int odinmetr = int.Parse(Console.ReadLine());
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Ну тогда до связи!");
+                //}
+
+                //Console.WriteLine("Сколько градусов в вашей комнате?");
+                //int answer = int.Parse(Console.ReadLine());
+                //if (answer >= 18)
+                //{
+                //    Console.WriteLine("Ваша температура в комнате выше 18 градусов");
+                //    string otvet = Console.ReadLine();
+                //}
