54fb186182d7a16be0d246bed2ba2f5646486935
Trunyashkaaaaaa <93324206+IljaTrunov@users.noreply.github.com>
2022-02-08 20:07:48 +0200

    Update Program.cs

--- CProgrammerimine2/Program.cs
+++ CProgrammerimine2/Program.cs
@@ -1,13 +1,13 @@
-﻿                // Console.WriteLine("Neighbors is out");
+
                 //}
                 //int plosad = 0;
-                //Console.WriteLine("Какая длина стены a?");
+                //Console.WriteLine("Длина первой стены ");
                 //int dlina1 = int.Parse(Console.ReadLine());
-                //Console.WriteLine("Какая длина стены b?");
+                //Console.WriteLine("Длина второй стены ?");
                 //int dlina2 = int.Parse(Console.ReadLine());
                 //plosad = dlina1 * dlina2;
                 //Console.WriteLine($"Площадь пола = {plosad}");
-                //Console.WriteLine("Хотели бы вы сделать ремонт?");
+                //Console.WriteLine("Ремонт запилить?");
                 //string remont = Console.ReadLine();
                 //if (remont.ToUpper() == "YES" || remont.ToUpper() == "ДА")
                 //{
@@ -16,7 +16,7 @@
                 //}
                 //else
                 //{
-                //    Console.WriteLine("Ну тогда до связи!");
+                //    Console.WriteLine("Пока!");
                 //}
 
                 //Console.WriteLine("Сколько градусов в вашей комнате?");
